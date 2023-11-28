616ecd81ab9badc2e0459a4f00cc754b41ffb462
Hozyain <qwpo01_0101@mail.ru>
2023-11-28 19:23:30 +0400

    Задание выполнено

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Cat {
     private String name;
     private int age;
@@ -35,5 +37,22 @@ public class Cat {
                 ", age=" + age +
                 ", color='" + color + '\'' +
                 '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Cat cat = (Cat) o;
+        return age == cat.age &&
+                Objects.equals(name, cat.name) &&
+                Objects.equals(color, cat.color);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, color);
     }
 }
